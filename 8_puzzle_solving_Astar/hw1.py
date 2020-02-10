34e3706228858abe105434cdeab3cb6bf0275982
shivamakhauri04 <shivamakhauri04@gmail.com>
2020-02-10 18:31:34 -0500

    psudocode

--- 8_puzzle_solving_Astar/hw1.py
+++ 8_puzzle_solving_Astar/hw1.py
@@ -1,3 +1,21 @@
+#Pseudocode
+# Input the initial matrix
+# Input the goal matrix
+# Create a graph node with the input
+# Calculate the f score of the start node
+# Expand the current node and append the expanded matrces each as a node to the graph in the Open list
+# Push the current node into closed list
+# Calculate the fsore again for each node
+# State with the least fscore is expanded again
+# Continue the above process till the current_state=goal_matrix
+
+
+class GraphNode:
+    def __init__(self,data,level,fvalue):
+        self.data = data
+        self.level = level
+        self.fvalue = fvalue
+
 class Astar:
     def main(self):
 
