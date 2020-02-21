57baf67e80fdfc51830c7346e9e7e261dadee357
shivamakhauri04 <shivamakhauri04@gmail.com>
2020-02-20 23:46:00 -0500

    updated the code for optimization

--- 8_puzzle_solving/hw1.py
+++ 8_puzzle_solving/hw1.py
@@ -1,13 +1,3 @@
-#Pseudocode
-# Input the initial matrix
-# Input the goal matrix
-# Create a graph node with the initial input matrix
-# Calculate the f score of the start node
-# Expand the current node and append the expanded matrces each as a node to the graph in the Open list
-# Push the current node into closed list
-# Calculate the fsore again for each node
-# State with the least fscore is expanded again
-# Continue the above process till the current_state=goal_matrix
 
 
 class GraphNode:
